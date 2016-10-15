599c37baa0413dc7822a1a89acf3083ab9d35dc9
rbaumbach <github@ryan.codes>
2016-10-15 16:03:55 -0700

    Initial project commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SpikeyMcSpikerson
+
+I'm tired of always web searching for junk I've done a million times.
+
+Time to keep some sort of a record...
---
+++ SpikeyMcSpikerson/ApplicationLaunchViewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class ApplicationLaunchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
